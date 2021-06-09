d32146af8f8aedcd84e7ff52eed9360af401675f
Alex Korzh <alexkorzh7@gmail.com>
2021-06-09 14:49:07 +0300

    Add mock data generator helper

---
+++ src/helpers/generateMockData.ts
@@ -0,0 +1,39 @@
+import { DateTime } from 'luxon';
+import { Tag, Team, Transaction } from '../types/transactions';
+
+const randomDateBetween = (start: DateTime, end: DateTime) => {
+	const date = DateTime.fromMillis(start.millisecond + Math.random() * (end.millisecond - start.millisecond));
+	return date.toISO();
+};
+
+const getRandomInt = (max: number) => {
+	return Math.floor(Math.random() * max);
+};
+
+const getRandomTags = (alltags: Tag[]) => {
+	let tagsCount = getRandomInt(alltags.length);
+	let tags = [];
+	while (tagsCount >= 0) {
+		const tag = alltags[getRandomInt(alltags.length)];
+		tags.push(tag);
+		tagsCount--;
+	}
+	tags = Array.from(new Set(tags));
+	return tags;
+};
+
+export const generateData = (numberOfTransactions: number, allTags: Tag[], allTeams: Team[]) => {
+	let counter = numberOfTransactions;
+	const data: Transaction[] = [];
+	while (counter >= 0) {
+		const transaction = {
+			date: randomDateBetween(DateTime.fromObject({}), DateTime.now()),
+			amountInCents: getRandomInt(10000),
+			tags: getRandomTags(allTags),
+			team: allTeams[getRandomInt(allTeams.length)],
+		};
+		data.push(transaction);
+		counter--;
+	}
+	return data;
+};
